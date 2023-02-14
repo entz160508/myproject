3dd0523bcd7e1c894d625b80ef7b3403f3962472
entz160508 <entz160508@gmail.com>
2023-02-14 12:39:35 +0900

    commit

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,5 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+	$IP = $_SERVER['REMOTE_ADDR'];
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -75,7 +76,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 		<p><a href="http://35.206.199.78/join/joinAccount">join</a></p>
 	</div>
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php $IP = $_SERVER['REMOTE_ADDR']; echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION  .'</strong>' : '';  echo '<strong> Your IP is : ' . $IP . '</strong>' ;  ?></p></div>
+	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php  echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION  .'</strong>' : '';  echo '<strong> Your IP is : ' . $IP . '</strong>' ;  ?></p></div>
 	
 </body>
 </html>
