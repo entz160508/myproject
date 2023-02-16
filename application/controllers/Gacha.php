8ffb5c07a0d27d0eae708e44ae55a8df28d36c8d
entz160508 <entz160508@gmail.com>
2023-02-16 11:54:51 +0900

    commit

--- application/controllers/Gacha.php
+++ application/controllers/Gacha.php
@@ -1,42 +1,41 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-public function __construct()
-{
-    parent::__construct();
-    $this->load->helper('form');
-    $this->load->helper('url');
-}
-
 class Gacha extends CI_Controller {
 
-        public function gachaView()
-        {
-                $this->load->view('gacha/gachaView');
+    public function __construct(){
+        parent::__construct();
+        $this->load->helper('form');
+        $this->load->helper('url');
+    }
+
+    public function gachaView()
+    {
+            $this->load->view('gacha/gachaView');
+    }
+
+    public function gachaCon(){
+        // $gachaid = $this->input->post("gachaid");
+        // var_dump($gachaid);
+
+        // console.log($('#gachaid').val());
+        // console.log($('#gachacount').val());
+        // console.log($('#count').val());
+
+        if ($this->input->post('submit') == true) {
+            $data['value1'] = $this->input->post('gachaid');
+            $data['value2'] = $this->input->post('gachacount');
+            $data['value3'] = $this->input->post('count')
+            $data['method'] = 'post';
         }
-
-        public function gachaCon(){
-            // $gachaid = $this->input->post("gachaid");
-            // var_dump($gachaid);
-
-            // console.log($('#gachaid').val());
-            // console.log($('#gachacount').val());
-            // console.log($('#count').val());
-
-            if ($this->input->post('submit') == true) {
-                $data['value1'] = $this->input->post('gachaid');
-                $data['value2'] = $this->input->post('gachacount');
-                $data['value3'] = $this->input->post('count')
-                $data['method'] = 'post';
-            }
-            else if($this->input->get('submit') == true){
-                $data['value1'] = $this->input->get('gachaid');
-                $data['value2'] = $this->input->get('gachacount');
-                $this['value3'] = $this->input->get('count')
-                $data['method'] = 'get';
-            }
-            $this->load->view('gacha/form_receive_view',$data);
+        else if($this->input->get('submit') == true){
+            $data['value1'] = $this->input->get('gachaid');
+            $data['value2'] = $this->input->get('gachacount');
+            $this['value3'] = $this->input->get('count')
+            $data['method'] = 'get';
         }
+        $this->load->view('gacha/form_receive_view',$data);
+    }
 
 
 }
